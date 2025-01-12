5aede23f32390ddca145c4aef3ff95c6bcf61709
test1xenon <test1xenon@gmail.com>
2025-01-12 23:37:59 +1100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
+app.use(cors());
 
 const app = express();
 const db = new sqlite3.Database('./database.db');
